5d8454089d0fa4e560ce4a2f84bf78d61792caf1
Paulina Grymulska <paulina.grymulska@gmail.com>
2021-11-29 16:58:08 +0100

    min value added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,67 +1,67 @@
 import { useState } from "react";
 import "./style.css";
-import {Result} from "./Result";
+import { Result } from "./Result";
 import { currencies } from "../currencies";
 
 export const Form = () => {
-    const [currency, setCurrency] = useState(currencies[0].short);
-    const [amount, setAmount] = useState("");
-    const [result, setResult] = useState();
+  const [currency, setCurrency] = useState(currencies[0].short);
+  const [amount, setAmount] = useState("");
+  const [result, setResult] = useState();
 
-    const calculateResult = () => {
-        const rate = currencies.find(({short}) => short === currency).rate;
+  const calculateResult = () => {
+    const rate = currencies.find(({ short }) => short === currency).rate;
 
-        setResult({
-            currency,
-            startAmount: +amount,
-            endAmount: amount / rate
-        });
-    };
+    setResult({
+      currency,
+      startAmount: +amount,
+      endAmount: amount / rate
+    });
+  };
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        calculateResult();
-    };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    calculateResult();
+  };
 
-    return (
-        <form className="form" onSubmit={onFormSubmit}>
-            <h1 className="form__header">Przelicznik walut</h1>
-            <p>
-                <label className="form__label">
-                    <span className="form__labelText">Kwota*:</span>
-                    <input
-                        value={amount}
-                        onChange={(event) => setAmount(event.target.value)}
-                        className="form__field"
-                        placeholder="Kwota w PLN"
-                        type="number"
-                        in="0.01"
-                        step="0.01"
-                        required
-                    />
-                </label>
-            </p>
-            <p>
-                <label className="form__label">
-                    <span className="form__labelText">Waluta*:</span>
-                    <select
-                        value={currency}
-                        onChange={(event) => setCurrency(event.target.value)}
-                        className="form__field"
-                        required>
-                        {currencies.map(currency => (
-                            <option value={currency.short} key={currency.short}>{currency.name}</option>
-                        ))}
-                    </select>
-                </label>
-            </p>
-            <p>
-                <button className="form__button">Przelicz!</button>
-            </p>
-            <Result result={result} />
-            <p>
-                * - pola wymagające uzupełniania
-            </p>
-        </form>
-    );
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <h1 className="form__header">Przelicznik walut</h1>
+      <p>
+        <label className="form__label">
+          <span className="form__labelText">Kwota*:</span>
+          <input
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+            className="form__field"
+            placeholder="Kwota w PLN"
+            type="number"
+            min="0.01"
+            step="0.01"
+            required
+          />
+        </label>
+      </p>
+      <p>
+        <label className="form__label">
+          <span className="form__labelText">Waluta*:</span>
+          <select
+            value={currency}
+            onChange={(event) => setCurrency(event.target.value)}
+            className="form__field"
+            required>
+            {currencies.map(currency => (
+              <option value={currency.short} key={currency.short}>{currency.name}</option>
+            ))}
+          </select>
+        </label>
+      </p>
+      <p>
+        <button className="form__button">Przelicz!</button>
+      </p>
+      <Result result={result} />
+      <p>
+        * - pola wymagające uzupełniania
+      </p>
+    </form>
+  );
 };
