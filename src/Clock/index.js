ea7868c299546e9df0b5efebe29be2b39e150d71
Paulina Grymulska <paulina.grymulska@gmail.com>
2021-12-02 14:05:45 +0100

    formatting

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -16,12 +16,12 @@ export const Clock = () => {
   const formatDate = date.toLocaleString(
     undefined,
     {
-      hour:"2-digit", 
-      minute:"2-digit", 
-      second:"2-digit", 
-      weekday: "long", 
-      day: "numeric", 
-      month: "long", 
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      weekday: "long",
+      day: "numeric",
+      month: "long",
       year: "numeric"
     },
   );
