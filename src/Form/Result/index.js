09b801af0f41cd634e1cc795e67065d68cf9dfbe
Paulina Grymulska <paulina.grymulska@gmail.com>
2021-12-02 15:43:38 +0100

    small-screens layout fixed

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -3,12 +3,10 @@ import "./style.css";
 export const Result = ({ result }) => {
   return (!!result &&
     (<p className="result">
-      {result.startAmount.toFixed(2)}
-      &nbsp;PLN&nbsp;=&nbsp;
+      {result.startAmount.toFixed(2)}&nbsp;PLN = 
       <strong>
-        {result.endAmount.toFixed(2)}
-        &nbsp;
-        {result.currency}
+        {" "}{result.endAmount.toFixed(2)}
+        &nbsp;{result.currency}
       </strong>
     </p>)
   )
