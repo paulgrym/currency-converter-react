1eeee7180832d1836b889b6cbfa1b5e6232e0951
Paulina Grymulska <paulina.grymulska@gmail.com>
2021-12-27 22:51:54 +0100

    changed setTimeout time, no data error fixed

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -6,14 +6,21 @@ export const useRatesDate = () => {
   useEffect(() => {
     const getRates = async () => {
       try {
-        const currenciesAPIUrl = 'https://api.exchangerate.host/latest?base=PLN&symbols=EUR,USD,CHF,GBP,JPY,CZK,AUD,CAD'
-        const response = await fetch(currenciesAPIUrl);
+        const currenciesAPIUrl = [
+          `https://api.exchangerate.host/latest?base=PLN&symbols=EUR,USD,CHF,GBP,JPY,CZK,AUD,CAD&v=${new Date().getTime()}`,
+          "./src/Form/ratesDataCash.json"
+        ];
+        const response = await fetch(currenciesAPIUrl[0]);
         if (!response.ok) {
           throw new Error(response.statusText);
         }
 
         const { date, rates } = await response.json();
         console.log({ date, rates });
+
+        if (!rates) {
+          throw new Error(response.statusText);
+        }
 
         setRatesData({
           date,
@@ -26,7 +33,7 @@ export const useRatesDate = () => {
         setRatesData({ state: "error" });
       }
     }
-    setTimeout(getRates, 1500);
+    setTimeout(getRates, 2_000);
   }, []);
 
   return ratesData;
