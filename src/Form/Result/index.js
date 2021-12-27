b2f0eead9109ccf9b50541948d84eb8288caf9f5
Paulina Grymulska <paulina.grymulska@gmail.com>
2021-12-27 22:49:52 +0100

    changed result display

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,13 +1,18 @@
 import { StyledResult } from "./styled";
 
 export const Result = ({ result }) => {
-  return (!!result &&
-    (<StyledResult>
-      {result.startAmount.toFixed(2)}&nbsp;PLN =
-      <strong>
-        {" "}{result.endAmount.toFixed(2)}
-        &nbsp;{result.currency}
-      </strong>
-    </StyledResult>)
+  if (result) {
+    return (
+      <StyledResult>
+        {result.startAmount.toFixed(2)}&nbsp;PLN =
+        <strong>
+          {" "}{result.endAmount.toFixed(2)}
+          &nbsp;{result.currency}
+        </strong>
+      </StyledResult>
+    )
+  }
+  return (
+    <StyledResult><br></br></StyledResult>
   )
 };
